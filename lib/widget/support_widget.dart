4950fa1153976904e648badb4bae11a7fb5abf2a
Prasad Madushanka <lahirumadushankap2000@gmail.com>
2025-01-25 13:20:24 +0530

    Upload widget Directory

---
+++ lib/widget/support_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle boldTextFieldStyle() {
+    return const TextStyle(
+        color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle LightTextFieldStyle() {
+    return const TextStyle(
+        color: Colors.black45, fontSize: 15.0, fontWeight: FontWeight.w500);
+  }
+
+  static TextStyle semiboldTextFieldStyle() {
+    return const TextStyle(
+        color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle semiboldTextFieldStyle_2() {
+    return const TextStyle(
+        color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle semiboldTextFieldStyle_3() {
+    return const TextStyle(
+        color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle smallboldTextFieldStyle() {
+    return const TextStyle(
+        color: Colors.black45, fontSize: 10.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle smallboldTextFieldStyle_2() {
+    return const TextStyle(
+        color: Colors.black45, fontSize: 7.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle smallboldTextFieldStyle_3() {
+    return const TextStyle(
+        color: Colors.black, fontSize: 7.0, fontWeight: FontWeight.bold);
+  }
+}
